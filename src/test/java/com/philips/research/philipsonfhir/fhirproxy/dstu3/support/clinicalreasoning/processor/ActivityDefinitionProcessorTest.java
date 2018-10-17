9eaf63b059246045e75592a268ade90e790a0ceb
CODE1\nly98977 <bas.van.den.heuvel@philips.com>
2018-10-17 09:28:27 +0200

    ProcedureRequest tests done.

--- src/test/java/com/philips/research/philipsonfhir/fhirproxy/dstu3/support/clinicalreasoning/processor/ActivityDefinitionProcessorTest.java
+++ src/test/java/com/philips/research/philipsonfhir/fhirproxy/dstu3/support/clinicalreasoning/processor/ActivityDefinitionProcessorTest.java
@@ -9,62 +9,143 @@ import org.hl7.fhir.dstu3.model.*;
 import org.hl7.fhir.dstu3.utils.StructureMapUtilities;
 import org.hl7.fhir.exceptions.FHIRException;
 import org.hl7.fhir.instance.model.api.IBaseResource;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import java.io.*;
 import java.util.Date;
 import java.util.stream.Collectors;
 
-import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.*;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 public class ActivityDefinitionProcessorTest {
     private static final DefaultProfileValidationSupport defaultProfileValidationSupport = new DefaultProfileValidationSupport();
+    private static FhirContext ourCtx;
+    private static StructureMapTransformServer structureMapTransformServer;
+    private static StructureMapUtilities structureMapUtilities;
 
-    @Test
-    public void testProcedureRequest() throws FHIRException, IOException {
-        FhirContext ourCtx = FhirContext.forDstu3();
+    @BeforeClass
+    public static void init(){
+        ourCtx = FhirContext.forDstu3();
 
         IGenericClient fhirClient = mock( IGenericClient.class );
         when( fhirClient.getFhirContext() ).thenReturn( ourCtx );
+        structureMapTransformServer = new StructureMapTransformServer( fhirClient );
 
-        fhirClient = ourCtx.newRestfulGenericClient( "http://localhost:9500/baseDstu3" );
+        structureMapUtilities = new StructureMapUtilities( new MyWorkerContext( ourCtx, defaultProfileValidationSupport ));
+    }
 
-        StructureMapTransformServer structureMapTransformServer = new StructureMapTransformServer( fhirClient );
+    @Test
+    public void testProcedureRequestFull() throws FHIRException, IOException {
+        String mapStr = "";
+        String mapJsonStr = "";
+        StructureMap structureMap = getStructureMap( mapStr );
 
+        // test 1 - apply basic parameters
+        ActivityDefinition activityDefinition = (ActivityDefinition) new ActivityDefinition(  )
+            .setTiming(  new Timing().addEvent( new Date( ) ) )
+            .setCode( new CodeableConcept().addCoding( new Coding( ).setCode( "2233" ).setSystem( "a" ) ) )
+            .addBodySite( new CodeableConcept().addCoding( new Coding( ).setSystem( "http://example.com").setCode( "someCode" ) ) )
+            .setId( "AdGenProcedureRequest" );
 
-        MyWorkerContext hapiWorkerContext = new MyWorkerContext( ourCtx, defaultProfileValidationSupport );
+        Parameters parameters = new Parameters()
+            .addParameter( new Parameters.ParametersParameterComponent(  )
+                .setName( "source" )
+                .setResource( activityDefinition )
+            )
+            .addParameter( new Parameters.ParametersParameterComponent(  )
+                .setName( "subject" )
+                .setValue( new Reference( "Patient/patientid" ) )
+            )
+            ;
 
-        StructureMapUtilities structureMapUtilities = new StructureMapUtilities(hapiWorkerContext );
+        IBaseResource result = structureMapTransformServer.doTransform( structureMap, parameters, null );
+        assertNotNull( result );
+        assertTrue( result instanceof ProcedureRequest );
+        ProcedureRequest procedureRequest = (ProcedureRequest)result;
+        assertEquals( "ActivityDefinition/"+activityDefinition.getId(), procedureRequest.getBasedOnFirstRep().getReference() );
+        assertEquals( "Patient/patientid", procedureRequest.getSubject().getReference() );
+        assertTrue( !procedureRequest.hasContext() );
+        assertTrue( !procedureRequest.hasRequester() );
+        // test PR contents
+        assertEquals( activityDefinition.getTiming(), procedureRequest.getOccurrence() );
+        assertEquals( ProcedureRequest.ProcedureRequestStatus.DRAFT, procedureRequest.getStatus() );
+        assertEquals( ProcedureRequest.ProcedureRequestIntent.ORDER, procedureRequest.getIntent() );
+        assertEquals( activityDefinition.getCode(), procedureRequest.getCode() );
+        assertEquals( activityDefinition.getBodySite(), procedureRequest.getBodySite() );
+
+        // add advanced parameters
+        parameters.addParameter( new Parameters.ParametersParameterComponent(  )
+            .setName( "encounter" )
+            .setValue( new Reference( "Encounter/encounterid" ) )
+        )
+            .addParameter( new Parameters.ParametersParameterComponent(  )
+                .setName( "practitioner" )
+                .setValue( new Reference( "Practioner/practionerId" ) )
+            )
+        ;
+
+        result = structureMapTransformServer.doTransform( structureMap, parameters, null );
+        assertNotNull( result );
+        assertTrue( result instanceof ProcedureRequest );
+        procedureRequest = (ProcedureRequest)result;
+        assertEquals( "ActivityDefinition/"+activityDefinition.getId(), procedureRequest.getBasedOnFirstRep().getReference() );
+        assertEquals( "Encounter/encounterid", procedureRequest.getContext().getReference() );
+        assertEquals( "Practioner/practionerId", procedureRequest.getRequester().getAgent().getReference() );
+        assertTrue( !procedureRequest.getRequester().hasOnBehalfOf() );
+
+        parameters.addParameter( new Parameters.ParametersParameterComponent(  )
+            .setName( "organization" )
+            .setValue( new Reference( "Organization/organizationId" ) )
+        );
+
+        result = structureMapTransformServer.doTransform( structureMap, parameters, null );
+        assertNotNull( result );
+        assertTrue( result instanceof ProcedureRequest );
+        procedureRequest = (ProcedureRequest)result;
+        assertEquals( "ActivityDefinition/"+activityDefinition.getId(), procedureRequest.getBasedOnFirstRep().getReference() );
+        assertEquals( "Encounter/encounterid", procedureRequest.getContext().getReference() );
+        assertEquals( "Practioner/practionerId", procedureRequest.getRequester().getAgent().getReference() );
+        assertEquals( "Organization/organizationId", procedureRequest.getRequester().getOnBehalfOf().getReference() );
+
+        // check errors
+        try {
+            parameters = new Parameters()
+                .addParameter( new Parameters.ParametersParameterComponent()
+                    .setName( "source" )
+                    .setResource( new ActivityDefinition().addDosage( new Dosage().setText( "aaa" ) ).setId( "AdGenProcedureRequest" ) )
+                )
+            ;
+
+            structureMapTransformServer.doTransform( structureMap, parameters, null );
+            fail("should have thrown exception");
+        }
+        catch ( FHIRException e ){}
+
+        try {
+            parameters = new Parameters()
+                .addParameter( new Parameters.ParametersParameterComponent()
+                    .setName( "source" )
+                    .setResource( new ActivityDefinition().setProduct( new Reference(  ).setReference( "Substance/sdakdjasl" ) ).setId( "AdGenProcedureRequest" ) )
+                )
+            ;
+
+            structureMapTransformServer.doTransform( structureMap, parameters, null );
+            fail("should have thrown exception");
+        }
+        catch ( FHIRException e ){}
+    }
+
+    @Test
+    public void testProcedureRequest() throws FHIRException, IOException {
+
 
         StructureMap structureMap;
         String mapStr = "";
         String mapJsonStr = "";
-        {
-            InputStream is = getClass().getClassLoader().getResourceAsStream( "dstu3/ProcedureRequest.fhirmap" );
-            if ( is != null ) {
-                BufferedReader reader = new BufferedReader( new InputStreamReader( is ) );
-                mapStr = reader.lines().collect( Collectors.joining( System.lineSeparator() ) );
-            }
-            structureMap = (StructureMap) structureMapUtilities.parse( mapStr ).setId("Ad2ProcedureRequest");
-        }
-
-//        {
-//            InputStream is = new FileInputStream( new File( "example/StructureMap-Ad2ProcedureRequest.json" ) );
-//            if ( is != null ) {
-//                BufferedReader reader = new BufferedReader( new InputStreamReader( is ) );
-//                mapJsonStr = reader.lines().collect( Collectors.joining( System.lineSeparator() ) );
-//            }
-//            structureMap = (StructureMap) ourCtx.newJsonParser().parseResource( mapJsonStr ).setId("Ad2ProcedureRequest");
-//        }
-
-//        InputStream is = getClass().getClassLoader().getResourceAsStream( "dstu3/ProcedureRequest.fhirmap" );
-//        if (is != null) {
-//            BufferedReader reader = new BufferedReader(new InputStreamReader(is));
-//            mapStr = reader.lines().collect( Collectors.joining(System.lineSeparator()));
-//        }
-
+        structureMap = getStructureMap( mapStr );
 
         ActivityDefinition activityDefinition = (ActivityDefinition) new ActivityDefinition(  )
             .setTiming(  new Timing().addEvent( new Date( ) ) )
@@ -110,6 +191,17 @@ public class ActivityDefinitionProcessorTest {
         IBaseResource result = structureMapTransformServer.doTransform( structureMap, parameters, null );
         assertNotNull( result );
         System.out.println(ourCtx.newJsonParser().setPrettyPrint( true ).encodeResourceToString( result ));
+    }
+
+    private StructureMap getStructureMap(String mapStr) throws FHIRException {
+        StructureMap structureMap;
+        InputStream is = getClass().getClassLoader().getResourceAsStream( "dstu3/ProcedureRequest.fhirmap" );
+        if ( is != null ) {
+            BufferedReader reader = new BufferedReader( new InputStreamReader( is ) );
+            mapStr = reader.lines().collect( Collectors.joining( System.lineSeparator() ) );
+        }
+        structureMap = (StructureMap) structureMapUtilities.parse( mapStr ).setId( "Ad2ProcedureRequest" );
+        return structureMap;
     }
 
 }
